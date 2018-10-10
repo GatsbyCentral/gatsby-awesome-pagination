2ccf2d107660aab98619ac8983a49be068f0e8c5
Callum Macdonald <git@callum-macdonald.com>
2018-10-10 18:55:28 +0200

    Some tests for createPagePerItem().

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,6 +1,8 @@
+import _ from "lodash";
+import fp from "lodash/fp";
 import { paginate, createPagePerItem } from "../src";
 
-describe("paginate", () => {
+describe("paginate()", () => {
   it("creates pages and forwards context", () => {
     const createPage = jest.fn();
     paginate({
@@ -25,6 +27,42 @@ describe("paginate", () => {
         pageNumber === 0 ? "/blog" : "/blog/page",
       component: "path/to/blog"
     });
+    expect(createPage).toMatchSnapshot();
+  });
+});
+
+const blogPosts = _.map(Array(43), (item, index) => ({
+  id: index,
+  title: `Post number ${index.toString()}`,
+  path: `/blog/post-${index + 1}`
+}));
+
+describe("createPagePerItem()", () => {
+  it("Calls createPage() once per item", () => {
+    const createPage = jest.fn();
+
+    createPagePerItem({
+      createPage,
+      items: fp.cloneDeep(blogPosts),
+      itemToPath: "path",
+      itemToId: "id",
+      component: "/path/to/some/template.js"
+    });
+
+    expect(createPage).toHaveBeenCalledTimes(blogPosts.length);
+  });
+
+  it("Calls createPage() with the correct params", () => {
+    const createPage = jest.fn();
+
+    createPagePerItem({
+      createPage,
+      items: fp.cloneDeep(blogPosts),
+      itemToPath: "path",
+      itemToId: "id",
+      component: "/path/to/some/template.js"
+    });
+
     expect(createPage).toMatchSnapshot();
   });
 });
